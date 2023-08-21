9c1c833be433e06f783ffdc920852b2ae78204a8
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-08-21 09:23:00 +0700

    .

--- string.java
+++ string.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
 
 public class string {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            long a = sc.nextInt();
+            int check = 0;
+            long f1 = 1, f2 = 1;
+            long fn;
+            if (t == 1 || t == 0)
+                check = 1;
+            for (int i = 3; i <= 92; i++) {
+                fn = f1 + f2;
+                f2 = f1;
+                f1 = fn;
+                if (a == fn) {
+                    check = 1;
+                }
+            }
+            if (check == 1) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
 }
--- test.java
+++ test.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+        mp.put(1,  1);
+        mp.put(2, 1);
+        int x = mp.get(1);
+        x++;
+        mp.put(1, x);
+        Set<Integer> st= mp.keySet();
+        for (int a : st) {
+            System.out.println(mp.get(a));
+        }
+
     }
 }
