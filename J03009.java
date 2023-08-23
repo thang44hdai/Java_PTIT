4440a6457172834bd81ceb60bd37aef5e5f6773a
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-08-23 17:57:24 +0700

    .

---
+++ J03009.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class J03009 {
+    public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        String space = sc.nextLine();
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            
+    }
+}
