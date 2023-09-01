4858182c604707dd5364bb2b7fc07e3abfd929a2
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-09-01 17:04:21 +0700

    .

---
+++ J04009.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class J04009 {
+    public static void main(String[] args) {
+        
+    }
+}
--- J07058.java
+++ J07058.java
@@ -21,10 +21,6 @@ class monThi {
 
 public class J07058 {
 
-    public static int cmp(monThi x, monThi y) {
-        return x.maMon.compareTo(y.maMon);
-    }
-
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(new File("MONHOC.in"));
         int n = sc.nextInt();
@@ -32,13 +28,16 @@ public class J07058 {
         Vector<monThi> vt = new Vector<>();
         for (int i = 0; i < n; i++)
             vt.add(new monThi(sc.nextLine(), sc.nextLine(), sc.nextLine()));
-        Collections.sort(vt, new Comparator<monThi>() {
-            @Override
-            public int compare(monThi a, monThi b) {
-                if (a.maMon.compareTo(b.maMon) > 0)
-                    return 1;
-                return -1;
+        vt.sort(new Comparator<monThi>() {
+            public int compare(monThi a, monThi b){
+                return a.maMon.compareTo(b.maMon);
             }
+            // @Override
+            // public int compare(monThi a, monThi b) {
+            //     if (a.maMon.compareTo(b.maMon) > 0)
+            //         return 1;
+            //     return -1;
+            // }
         });
         for (int i = 0; i < n; i++)
             System.out.println(vt.get(i));
