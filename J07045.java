888a5d15b1130d4ef13612676e6430ee4cadae49
thang44hdai <thangquang8888@gmail.com>
2023-11-15 17:19:06 +0700

    .

--- J07045.java
+++ J07045.java
@@ -1,44 +1,44 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
 
-class loaiphong {
+class LoaiPhong implements Comparable<LoaiPhong> {
     String kh, name;
     String dongia, phi;
 
-    loaiphong(String kh, String name, String dongia, String phi) {
-        this.kh = kh;
-        this.name = name;
-        this.dongia = dongia;
-        this.phi = phi;
+    LoaiPhong(String s) {
+        String f[] = s.trim().split(" ");
+        this.kh = f[0];
+        this.name = f[1];
+        this.dongia = f[2];
+        this.phi = f[3];
     }
 
     public String toString() {
         return this.kh + " " + this.name + " " + this.dongia + " " + this.phi;
     }
+
+    public int compareTo(LoaiPhong a) {
+        return this.name.compareTo(a.name);
+    }
 }
 
 public class J07045 {
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(new File("PHONG.in"));
-        int n = sc.nextInt();
-        loaiphong a[] = new loaiphong[n];
-        sc.nextLine();
-        for (int i = 0; i < n; i++) {
-            String f[] = sc.nextLine().trim().split(" ");
-            a[i] = new loaiphong(f[0], f[1], f[2], f[3]);
+    public static void main(String[] args) throws IOException {
+        ArrayList<LoaiPhong> ds = new ArrayList<>();
+        Scanner in = new Scanner(new File("PHONG.in"));
+        int n = Integer.parseInt(in.nextLine());
+        while (n-- > 0) {
+            ds.add(new LoaiPhong(in.nextLine()));
         }
-        Arrays.sort(a, 0, n, new Comparator<loaiphong>() {
-            @Override
-            public int compare(loaiphong o1, loaiphong o2) {
-                return o1.name.compareTo(o2.name);
-            }
-        });
-
-        for (loaiphong i : a)
-            System.out.println(i);
+        Collections.sort(ds);
+        for (LoaiPhong tmp : ds) {
+            System.out.println(tmp);
+        }
     }
 }
